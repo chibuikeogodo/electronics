cefa54f62b5bf9a6c2d5ebe552137717a007a492
ogodocity <ogodochibuike@gmail.com>
2023-08-19 19:21:07 +0100

    comit the next one

--- electronics/settings.py
+++ electronics/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'electronics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'electroadb',
+        'USER': 'root',
+        'PASSWORD': 'nigeria111',
+        'HOST': '127.0.0.1',
     }
 }
 
